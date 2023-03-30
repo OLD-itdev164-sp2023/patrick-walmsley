187b1b3aed5fa46bf01f1f2af362bc8b8d0a76dc
walmslea <98186467+walmslea@users.noreply.github.com>
2023-03-30 12:17:16 -0500

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -11,12 +11,13 @@ const IndexPage = ({ data }) => (
     <Seo title="Home" />
     <ul className={styles.list}>
       {
-        data.allContentfullBlogPost.edges.map(edge => (
+        data.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
             <div>
-              <Gatsby Image
+              <GatsbyImage
                 image={edge.node.heroImage.gatsbyImageData}
+                alt=""
               />
             </div>
             <div>
@@ -28,23 +29,26 @@ const IndexPage = ({ data }) => (
     </ul>
   </Layout>
 )
+export const Head = () => <Seo title="Home" />
 
-export const query = graphl`
+export default IndexPage;
+
+export const query = graphql`
   {
     allContentfulBlogPost {
       edges {
         node {
-          id
+          id 
           title
-          slug
+          slug 
           body {
-            childMarkdownRequest {
-              excerpt
+						childMarkdownRemark {
+							excerpt
             }
           }
-          heroImage{
-            gastbyImageData(
-              layout: CONSTRAINED
+          heroImage {
+						gatsbyImageData(
+							layout: CONSTRAINED
               placeholder: BLURRED
               width: 300
             )
--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -2,26 +2,27 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-const BlogPost = ({ data }) => (
+const BlogPost = ({ data }) => {
     const { title, body } = data.contentfulBlogPost;
 
     return(
         <Layout>
             <h1>{title}</h1>
-            <div dangerouslySetInnerHTML={{__html: body.childMarkdownRequest.html}}></div>
+            <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
     );
-)
+    
+}
 
 export default BlogPost;
 
-export const pageQuery = graphql`
+const pageQuery = graphql`
     query blogPostQuery($slug: String!) {
         contentfulBlogPost(slug: {eq: $slug}) {
             title
             slug
             body {
-                childMarkdownRequest{
+                childMarkdownRemark{
                     html
                 }
             }
